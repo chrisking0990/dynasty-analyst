20cce4091e3c1310e4a57f1d5188648c97c47aca
chrisking0990 <chrisking0990@gmail.com>
2026-03-07 18:58:47 -0600

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from '@clerk/nextjs';
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+// THIS IS WHAT CONTROLS THE BROWSER TAB!
+export const metadata: Metadata = {
+  title: "DynastyAnalyst",
+  description: "AI-Powered Dynasty Fantasy Football Trade Machine & Advanced Analytics",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
